fcc02f876356769ca09d716fda7ba4862504e7dc
jforbes <jforbes@brightcove.com>
2015-03-20 11:23:40 -0400

    Extracted e2e testing task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,15 +10,19 @@ module.exports = function(grunt) {
     // , 'protractor:localDevices' // TODO: make devices stable
   ]);
 
+  grunt.registerTask('start-device-server', [
+    'appium',
+    'proxyDevice:79431df8dc364454f4850ceacb447797bc313574' //Mobile152]
+  ]);
+  grunt.registerTask('e2e-test', ['connect', (process.env.JENKINS ? 'concurrent:ci' : 'localSerial')]);
+
   // TODO: setup Jenkins, find real env variable
   grunt.registerTask('test', [
     'setup',
     'jshint',
     'clean',
     'copy',
-    'appium',
-    'proxyDevice:79431df8dc364454f4850ceacb447797bc313574', //Mobile152
-    'connect',
-    (process.env.JENKINS ? 'concurrent:ci' : 'localSerial')
+    'start-device-server',
+    'e2e-test'
   ]);
 };
