68b52fa1e95b3d33e3022af8d5dc78aa0c7af6f2
jforbes <jforbes@brightcove.com>
2015-03-30 14:29:07 -0400

    Added iOS capabilities

--- test/sauce.config.js
+++ test/sauce.config.js
@@ -1,71 +1,99 @@
 var browsers = [{
-  browserName: 'internet explorer',
-  platform: 'Windows 7',
-  version: '8'
-}, {
-  browserName: 'internet explorer',
-  platform: 'Windows 7',
-  version: '9'
-}, {
-  browserName: 'internet explorer',
-  platform: 'Windows 7',
-  version: '10'
-}, {
-  browserName: 'internet explorer',
-  platform: 'Windows 7',
-  version: '11'
-}, {
-  browserName: 'internet explorer',
-  platform: 'Windows 8.1',
-  version: '11'
-}, {
-  browserName: 'chrome',
-  version: '41',
-  platform: 'OSX 10.10'
-}, {
-  browserName: 'chrome',
-  version: '40',
-  platform: 'OSX 10.10'
-}, {
-  browserName: 'safari',
-  version: '6',
-  platform: 'OSX 10.8'
-}, {
-  browserName: 'safari',
-  version: '7',
-  platform: 'OSX 10.9'
-}, {
-  browserName: 'safari',
-  version: '8',
-  platform: 'OSX 10.10'
-}, {
-  browserName: 'firefox',
-  version: '36',
-  platform: 'OSX 10.10'
-}, {
-  browserName: 'firefox',
-  version: '35',
-  platform: 'OSX 10.10'
-}, {
-  browserName: 'firefox',
-  version: '34',
-  platform: 'OSX 10.10'
-}].map(function(e) {
-  return {
-    browserName: e.browserName,
-    version: e.version,
-    platform: e.platform,
-    name: e.browserName + '-' + e.version + '-' + e.platform,
-    build: process.env.TRAVIS_BUILD_NUMBER,
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-  };
-});
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '8'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '9'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '10'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '11'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 8.1',
+    version: '11'
+  }, {
+    browserName: 'chrome',
+    version: '41',
+    platform: 'OSX 10.10'
+  }, {
+    browserName: 'chrome',
+    version: '40',
+    platform: 'OSX 10.10'
+  }, {
+    browserName: 'safari',
+    version: '6',
+    platform: 'OSX 10.8'
+  }, {
+    browserName: 'safari',
+    version: '7',
+    platform: 'OSX 10.9'
+  }, {
+    browserName: 'safari',
+    version: '8',
+    platform: 'OSX 10.10'
+  }, {
+    browserName: 'firefox',
+    version: '36',
+    platform: 'OSX 10.10'
+  }, {
+    browserName: 'firefox',
+    version: '35',
+    platform: 'OSX 10.10'
+  }, {
+    browserName: 'firefox',
+    version: '34',
+    platform: 'OSX 10.10'
+  }].concat(
+    [{
+      platformVersion: '8.1',
+      deviceName: 'iPhone Simulator'
+    }, {
+      platformVersion: '8.1',
+      deviceName: 'iPad Simulator'
+    }, {
+      platformVersion: '7.1',
+      deviceName: 'iPhone Simulator'
+    }, {
+      platformVersion: '7.1',
+      deviceName: 'iPad Simulator'
+    }, {
+      platformVersion: '6.1',
+      deviceName: 'iPhone Simulator'
+    }, {
+      platformVersion: '6.1',
+      deviceName: 'iPad Simulator'
+    }].map(function(e) {
+      return {
+        platformVersion: e.platformVersion,
+        deviceName: e.deviceName,
+        browserName: 'safari',
+        platformName: 'iOS',
+        nativeWebTap: true
+      };
+    }))
+  .map(function(e) {
+    return {
+      browserName: e.browserName,
+      version: e.version || '',
+      platform: e.platform || '',
+      name: e.browserName + '-' + e.version + '-' + e.platform,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+    };
+  });
 
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-  maxSessions: 1,
+  maxSessions: 3,
 
   multiCapabilities: browsers,
 
