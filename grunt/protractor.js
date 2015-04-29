ed917156d191827a148d713ccf1760c743c2ed52
jforbes <jforbes@brightcove.com>
2015-04-29 19:31:04 -0400

    Fixed protractor task

--- grunt/protractor.js
+++ grunt/protractor.js
@@ -1,23 +1,25 @@
-module.exports = {
-  options: {
-    args: {
-      baseUrl: 'http://<%= page_hostname %>:' + (grunt.option('port') || 8080) + '/',
-      specs: ['dist/specs.js']
-    }
-  },
-  browsers: {
+module.exports = function(grunt) {
+  return {
     options: {
-      configFile: 'test/browsers.config.js'
+      args: {
+        baseUrl: 'http://<%= page_hostname %>:' + (grunt.option('port') || 8080) + '/',
+        specs: ['dist/specs.js']
+      }
+    },
+    browsers: {
+      options: {
+        configFile: 'test/browsers.config.js'
+      }
+    },
+    devices: {
+      options: {
+        configFile: 'test/devices.config.js'
+      }
+    },
+    sauce: {
+      options: {
+        configFile: 'test/saucelabs.config.js'
+      }
     }
-  },
-  devices: {
-    options: {
-      configFile: 'test/devices.config.js'
-    }
-  },
-  sauce: {
-    options: {
-      configFile: 'test/saucelabs.config.js'
-    }
-  }
+  };
 };
